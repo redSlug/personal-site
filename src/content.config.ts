bcd5e76673b713afb7cba0d7841fc75fd2ba9934
Gage-K <krausegage@gmail.com>
2025-04-21 14:20:18 -0400

    Implement work and projects collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,45 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+const projects = defineCollection({
+  // Load Markdown and MDX files in the `src/content/projects/` directory
+  loader: glob({ base: "./src/content/projects", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    link: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    // Transform string to Date object
+    date: z.coerce.date().optional(),
+  }),
+});
+
+const work = defineCollection({
+  // Load Markdown and MDX files in the `src/content/projects/` directory
+  loader: glob({ base: "./src/content/work", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    role: z.string(),
+    organization: z.string(),
+    location: z.string().optional(),
+    // Transform string to Date object
+    dateStart: z.coerce.date(),
+    dateEnd: z.coerce.date().optional(),
+  }),
+});
+
+export const collections = { blog, work, projects };
